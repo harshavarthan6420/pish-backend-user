d99852c7f380b079ca8b2a89e7f0dad25e0ba1f9
harshavarthan6420 <harshavarthan6420@gmail.com>
2024-04-12 19:57:00 +0100

    fixes

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const dotenv=require('dotenv').config()
 const cors=require('cors')
 const mongoose=require('mongoose')
 const app=express();
-
+const corsOptionsDelegate = function (req, callback) {
+    const corsOptions = { credentials: true }; 
+    if (['http://localhost:3000', 'https://pish.onrender.com'].indexOf(req.header('Origin')) !== -1) {
+      corsOptions.origin = req.header('Origin'); 
+    } else {
+      corsOptions.origin = false; 
+    }
+    callback(null, corsOptions);
+  };
+  
+  app.use(cors(corsOptionsDelegate));
 
 mongoose.connect("mongodb+srv://user:user@users.h5esfgv.mongodb.net/?retryWrites=true&w=majority")
 .then(()=> console.log('Database connected'))
 .catch((err)=> console.log('Database not connected',err))
 
 app.use(express.json())
-const corsOptions = {
-    origin: ['http://localhost:3000', 'https://pish.onrender.com'], 
-    credentials: true,
-  };
-app.use(cors(corsOptions));
+
 app.use('/',require('./routes/authRoutes'))
 
 const port = 3002;
